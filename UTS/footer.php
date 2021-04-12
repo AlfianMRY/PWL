669883ffab4ed75a2286ef2eb56d010ce0f6af75
AlfianMRY <72691066+AlfianMRY@users.noreply.github.com>
2021-04-12 15:45:46 +0700

    Add files via upload

---
+++ UTS/footer.php
@@ -0,0 +1,4 @@
+</div>
+<div class="alert alert-info text-center" role="alert">
+    Design By Alfian &copy; 2021
+</div>
---
+++ UTS/logout.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+//session_destroy();
+unset($_SESSION['MEMBER']);
+//landing page
+header('Location:http://localhost/UTS/index.php?hal=home');   
+?>
